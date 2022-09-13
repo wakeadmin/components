a8b9dd7ca8a334abc803ceab5cd969dc7c15b436
ivan <carney520@hotmail.com>
2022-09-13 16:59:41 +0800

    chore: 调整发布脚本
    Change-Id: I248eef38a5a805b66c9cd5b6469b54c4b93c02c1

--- scripts/publish-public.sh
+++ scripts/publish-public.sh
@@ -4,9 +4,6 @@
 
 # 需要配置的环境变量
 # NPM_TOKEN npm 用户名
-# DOCKER_USER docker 用户
-# DOCKER_PASSWORD docker 用户密码
-# DOCKER_SERVER docker 仓库, 可选，默认为 docker hub
 
 set -e
 set -x
@@ -32,9 +29,3 @@ npm config set //registry.npmjs.org/:_authToken $NPM_TOKEN
 # 发布 npm
 echo '开始发布 npm 模块'
 pnpm publish -r --no-git-checks --registry=$NPM_REGISTRY --access public
-
-# 构建基座容器
-echo '开始构建基座'
-pnpm run -r build:assets
-pnpm run -r build:docker
-pnpm run -r publish:docker
--- scripts/publish.sh
+++ scripts/publish.sh
@@ -7,9 +7,6 @@
 # NPM_PASS npm 密码
 # NPM_EMAIL npm 邮箱
 # NPM_REGISTRY npm 仓库
-# DOCKER_USER docker 用户
-# DOCKER_PASSWORD docker 用户密码
-# DOCKER_SERVER docker 仓库, 可选，默认为 docker hub
 
 set -e
 set -x
@@ -35,9 +32,3 @@ npm-cli-login
 # 发布 npm
 echo '开始发布 npm 模块'
 pnpm publish -r --no-git-checks --registry=$NPM_REGISTRY --access public
-
-# 构建基座容器
-echo '开始构建基座'
-pnpm run -r build:assets
-pnpm run -r build:docker
-pnpm run -r publish:docker
