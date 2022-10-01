149b671a52d6e5ae7f4cf501d99e1dc491c3b788
黎贵奇 <carney520@hotmail.com>
2022-10-01 20:40:42 +0800

    doc: fat-form-query 文档
    Change-Id: I8032bc88ef1b852b6a277989c7e1822c0ea5bd14

---
+++ packages/doc/docs/fat-form-layout/Query.vue
@@ -0,0 +1,20 @@
+<template>
+  <FatFormQuery>
+    <FatFormItem prop="search" value-type="search" label="请输入关键字" />
+    <FatFormItem
+      prop="status"
+      value-type="select"
+      :value-props="{
+        options: [
+          { label: '已开启', value: 1 },
+          { label: '未开启', value: 0 },
+        ],
+      }"
+      label="状态"
+    />
+  </FatFormQuery>
+</template>
+
+<script setup lang="tsx">
+  import { FatFormQuery, FatFormItem } from '@wakeadmin/components';
+</script>
---
+++ packages/doc/docs/fat-form-layout/query.md
@@ -0,0 +1,35 @@
+<script setup>
+  import Query from './Query.vue'
+</script>
+
+# FatFormQuery 查询 (beta)
+
+`FatFormQuery` 是 FatForm 针对查询表单场景设计的一个组件。 [FatTable](../fat-table/index.md) 内部就使用了 FatFormQuery。
+
+<br>
+<br>
+<br>
+
+## 示例
+
+<ClientOnly>
+  <div class="wk-demo"><Query /></div>
+</ClientOnly>
+
+::: details 查看代码
+<<< @/fat-form-layout/Query.vue
+:::
+
+<br>
+<br>
+<br>
+
+## API
+
+<br>
+
+![](./images/fat-form-query.png)
+
+<br>
+<br>
+<br>
