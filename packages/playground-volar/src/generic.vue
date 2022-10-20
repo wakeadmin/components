1413e7b54f099d4ef0aee60bf4314f716fe48f35
黎贵奇 <carney520@hotmail.com>
2022-10-20 16:40:58 +0800

    chore: 新增 volar 游乐场
    Change-Id: Ifebf122ce062ee7ea9aa0229432f75f4a7b93509

---
+++ packages/playground-volar/src/generic.vue
@@ -0,0 +1,19 @@
+<template>
+  <MyGenericComponent value="1" @change="value => {}">
+    <template #label="scope">{{ expectScope(scope) }}</template>
+  </MyGenericComponent>
+</template>
+
+<script setup lang="tsx">
+  /* eslint-disable no-empty-function */
+
+  const expectScope = (scoped: { foo: number; value: string }) => {};
+
+  const MyGenericComponent: new <T>(props: { value: T }) => {
+    $props: typeof props;
+    $emit: { (event: 'change', e: T): void };
+    $slots: {
+      label: (value: { value: T; foo: number }) => any;
+    };
+  } = {} as any;
+</script>
