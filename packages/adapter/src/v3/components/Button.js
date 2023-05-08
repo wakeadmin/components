175f28671f89c937218395760f281d2a4986a33d
黎贵奇 <carney520@hotmail.com>
2023-05-09 04:02:53 +0800

    fix: 修复 element-plus Button type=text 警告
    Change-Id: Ia0f223e8b48c70ba2289d1420f4f89b44fb2bda9

--- packages/adapter/src/v3/components/Button.js
+++ packages/adapter/src/v3/components/Button.js
@@ -1,3 +1,15 @@
 import { ElButton } from 'element-plus';
+import { h } from '@wakeadmin/demi';
 
-export { ElButton as Button };
+export const Button = (props, context) => {
+  const overrideProps = {};
+
+  // element-plus type=text 已废弃，使用 link 代替
+  // 不要使用 text! link 更接近旧版的 type=text 样式
+  if (props.type === 'text') {
+    overrideProps.type = 'primary';
+    overrideProps.link = true;
+  }
+
+  return h(ElButton, { ...props, ...overrideProps }, context.slots);
+};
